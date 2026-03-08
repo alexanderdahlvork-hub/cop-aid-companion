d71f71c98f937ccc7314bcfbe74cebbe904944e9
Alexander Dahl Vork <alexanderdahlvork@gmail.com>
2026-03-08 21:20:38 +0100

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-const devPlugins = [react(), componentTagger()];
-const prodPlugins = [react()];
-
-export default defineConfig(({ mode }) => ({
-  plugins: mode === "development" ? devPlugins : prodPlugins,
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
 
